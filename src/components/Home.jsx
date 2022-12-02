470f49ae1fcea3ccd42b0b6fee3582a75f72953b
Tooba <jtooba70@gmail.com>
2022-12-02 21:09:42 +0500

    update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,9 @@
 export default function Home(props) {
     return(
-        <div>
-            <h1>Quizzical</h1>
-            <button onClick={props.toggleStart}>Start Quiz</button>
+        <div className="home-screen">
+            <h1 className="quizzical">Quizzical</h1>
+            <p className="desc">A fun quiz game</p>
+            <button className="btn-start" onClick={props.toggleStart}>Start Quiz</button>
         </div>
     )
 }
